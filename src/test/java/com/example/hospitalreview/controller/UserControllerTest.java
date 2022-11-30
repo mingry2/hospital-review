963a3b4820b80b6f08461ab399b4f32d951070ff
minkyoung kim <minry20415@naver.com>
2022-11-30 22:36:28 +0900

    Test Code 추가

--- src/test/java/com/example/hospitalreview/controller/UserControllerTest.java
+++ src/test/java/com/example/hospitalreview/controller/UserControllerTest.java
@@ -13,16 +13,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+// test 코드에서는 카멜코드를 잘 사용하지않음 -> 가독성 떨어짐 (_언더바를 통해 사용)
 @WebMvcTest
 class UserControllerTest {
     @Autowired
@@ -33,9 +36,15 @@ class UserControllerTest {
 
     @Autowired
     ObjectMapper objectMapper;
+    UserJoinRequest userJoinRequest = UserJoinRequest.builder()
+            .userName("mingyeong")
+            .password("1q2w3e4r")
+            .email("mingyeong@naver.com")
+            .build();
 
     @Test
     @DisplayName("회원가입 성공")
+    @WithMockUser // 가상의 유효한 User 설정
     void join_success() throws Exception {
         UserJoinRequest userJoinRequest = UserJoinRequest.builder()
                 .userName("mingyeong")
@@ -45,6 +54,7 @@ class UserControllerTest {
         when(userService.join(any())).thenReturn(mock(UserDto.class));
 
         mockMvc.perform(post("/api/v1/users/join")
+                        .with(csrf()) // csrf() token 인증 통과
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsBytes(userJoinRequest)))
                 .andDo(print())
@@ -52,20 +62,54 @@ class UserControllerTest {
     }
 
     @Test
-    @DisplayName("회원가입 실패")
+    @DisplayName("회원가입 실패 - id 중복")
+    @WithMockUser
     void join_fail() throws Exception {
-        UserJoinRequest userJoinRequest = UserJoinRequest.builder()
-                .userName("mingyeong")
-                .password("12a34b")
-                .email("mingyeong@naver.com")
-                .build();
+
+        String password = "1q2w3e4r";
 
         when(userService.join(any())).thenThrow(new HospitalReviewAppException(ErrorCode.DUPLICATED_USER_NAME, ""));
 
         mockMvc.perform(post("/api/v1/users/join")
+                        .with(csrf())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsBytes(userJoinRequest)))
                 .andDo(print())
                 .andExpect(status().isConflict());
+    }
+
+    @Test
+    @DisplayName("로그인 실패 - id 없음")
+    @WithMockUser // 가상의 유효한 User 설정
+    void login_fail1() throws Exception {
+
+//        String id = "mingyeong";
+//        String password = "1q2w3e4r";
+
+        // 무엇을 보내서? : id, pw
+        when(userService.login(any(), any())).thenThrow(new HospitalReviewAppException(ErrorCode.NOT_FOUND, ""));
+        // 무엇을 받을까? : NOT_FOUND
+        // NOT_FOUND가 나오면 잘 나온 것
+        mockMvc.perform(post("/api/v1/users/join")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(userJoinRequest)))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+
+    }
+
+    @Test
+    @DisplayName("로그인 실패 - password 잘 못 입력")
+    @WithMockUser // 가상의 유효한 User 설정
+    void login_fail2() throws Exception {
+
+    }
+
+    @Test
+    @DisplayName("로그인 성공")
+    @WithMockUser // 가상의 유효한 User 설정
+    void login_success() throws Exception {
+
     }
 }
